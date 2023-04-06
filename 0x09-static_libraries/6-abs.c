157880497e2c1bc657bd52c8c09497495140d254
gbomiye <temiodesola@>
2023-04-06 06:47:17 -0700

    taskses

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of a whole number (integer)
+ * @n: the value to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
