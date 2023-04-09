133428201e644a804ba247fc480585b8c6b007ae
gbomiye <temiodesola@>
2023-04-09 01:51:59 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - print program name
+ *@argc: argument count
+ *@argv: argument vector
+ *auth: gbotemi
+ *
+ *Return:always 0
+ */
+
+int main(int argic __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+0x0A. C - argc, argv
+argument count & argument vector 
