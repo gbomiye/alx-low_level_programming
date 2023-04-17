7a11283e9ecb82af65a6f510f770c3e1341c2a84
gbomiye <temiodesola@>
2023-04-17 12:44:14 -0700

    task con

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - prints the name of the file it was compiled from
+ *auth - gbotemi
+ *returns:the file name
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+	return (0);
+
+}
