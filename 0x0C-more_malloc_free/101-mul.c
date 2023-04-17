2967725015be3bf8064e217b685afeacf9e97025
gbomiye <temiodesola@>
2023-04-17 13:31:02 -0700

    01

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -100,16 +100,17 @@ for (i = 0; i < len - 1; i++)
 {
 if (result[i])
 a = 1;
+}
 
 if (a)
-	_putchar(result[i] + '0');
+_putchar(result[i] + '0');
 
-}
 
 if (!a)
-{_putchar('0');
+_putchar('0');
 _putchar('\n');
-}
+
+
 free(result);
 
 return (0);
