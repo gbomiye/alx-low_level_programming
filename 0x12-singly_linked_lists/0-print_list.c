0b300e6c2253ccb6e41f30d030006990b4272796
gbomiye <temiodesola@gmail.com>
2023-04-27 05:11:07 -0700

    aa

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-size_t s = 0;
+size_t list = 0;
 
 	while (h)
 	{
@@ -19,8 +19,8 @@ size_t s = 0;
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		list++;
 }
 
-return (s);
+return (list);
 }
